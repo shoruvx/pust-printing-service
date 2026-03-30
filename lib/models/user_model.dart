96eea08378075e1637d831e7122ac269957b1472
MD Shahidur Rahman <166603336+shoruvx@users.noreply.github.com>
2026-03-30 14:55:31 +0600

    Create User data model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,15 @@
+class User {
+  final String rollNumber;
+  final String registrationNumber;
+  final String phone;
+  final String email;
+  final String password;
+
+  User({
+    required this.rollNumber,
+    required this.registrationNumber,
+    required this.phone,
+    required this.email,
+    required this.password,
+  });
+}
